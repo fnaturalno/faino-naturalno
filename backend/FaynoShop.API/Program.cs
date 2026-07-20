6a0a3b57148197f732fac5458a6712024044daea
Volodymyr Milovanov <volodymyr.milovanov@logity.tech>
2026-07-20 13:52:48 +0300

    Add migration

--- backend/FaynoShop.API/Program.cs
+++ backend/FaynoShop.API/Program.cs
@@ -49,6 +49,7 @@ using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
     var config = scope.ServiceProvider.GetRequiredService<IConfiguration>();
+    await db.Database.MigrateAsync();
     await SeedData.SeedAsync(db, config);
 }
 
